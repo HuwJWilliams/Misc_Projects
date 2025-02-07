1b1fec6aab59e24d0cb149db1818929188ab58fb
Huw <=>
2025-02-07 10:49:44 +0000

    Initial commit

---
+++ Snake_Game/main.py
@@ -0,0 +1,60 @@
+# %%
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+# Screen setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake(colour="white", shape="square")
+food = Food()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.right, "d")
+screen.onkey(snake.left, "a")
+
+screen.update()
+
+game_is_on = True
+while game_is_on == True:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move_forwards()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.update_score()
+
+    # Detect collision with wall
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 260
+        or snake.head.ycor() < -280
+    ):
+        scoreboard.game_over()
+        game_is_on = False
+
+    # Detect collision with tail
+    # if head collides with any segment in the tail:
+    for segment in snake.segment_ls[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
+
+# Screen Exitw
+screen.exitonclick()
---
+++ Snake_Game/snake.py
@@ -0,0 +1,76 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(
+        self,
+        colour: str = "white",
+        shape: str = "square",
+        move_distance: int = MOVE_DISTANCE,
+    ):
+
+        self.segment_ls = []
+        self.shape = shape
+        self.colour = colour
+        self.move_distance = move_distance
+        self.snake_heading = 0
+
+        self.make_init_snake()
+
+        self.head = self.segment_ls[0]
+
+    def make_body_piece(self, xy_coordinate: tuple = (0, 0)):
+        t = Turtle()
+        t.penup()
+        t.color(self.colour)
+        t.shape(self.shape)
+        t.setpos(xy_coordinate)
+        t.setheading(self.snake_heading)
+
+        return t
+
+    def make_init_snake(
+        self,
+    ):
+        for i in range(3):
+            xy_coordinate = (-20 * i, 0)
+            t = self.make_body_piece(xy_coordinate)
+            self.segment_ls.append(t)
+        return self.segment_ls
+
+    def move_forwards(self):
+
+        for seg_num in range(len(self.segment_ls) - 1, 0, -1):
+            new_x = self.segment_ls[seg_num - 1].xcor()
+            new_y = self.segment_ls[seg_num - 1].ycor()
+            self.segment_ls[seg_num].goto(new_x, new_y)
+        self.head.forward(self.move_distance)
+
+        return self.segment_ls
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+            self.snake_heading = 90
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+            self.snake_heading = 270
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+            self.snake_heading = 0
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+            self.snake_heading = 180
+
+    def extend(self):
+        xy_coordinate = self.segment_ls[-1].position()
+        t = self.make_body_piece(xy_coordinate)
+        self.segment_ls.append(t)
