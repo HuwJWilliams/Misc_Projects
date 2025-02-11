b45646abbf99a80aaa4226199fd6bd464830787f
Huw <=>
2025-02-11 15:11:40 +0000

    Add email automator

---
+++ Email_Automator/main.py
@@ -0,0 +1,11 @@
+with open("./input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+with open("./input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+
+for name in names:
+    name = name.strip("\n")
+    new_letter = letter.replace("[name]", name)
+    with open(f"./output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
+        file.write(new_letter)
