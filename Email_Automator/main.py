f8358b9b394d5049fafb1e827bcbf51e05a9bf71
Huw <=>
2025-02-12 10:14:16 +0000

    test

--- Email_Automator/main.py
+++ Email_Automator/main.py
@@ -1,5 +1,3 @@
-PLACEHOLDER = "[name]"
-
 with open("./input/Names/invited_names.txt") as file:
     names = file.readlines()
 
@@ -8,6 +6,6 @@ with open("./input/Letters/starting_letter.txt") as file:
 
 for name in names:
     name = name.strip("\n")
-    new_letter = letter.replace(PLACEHOLDER, name)
+    new_letter = letter.replace("[name]", name)
     with open(f"./output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
         file.write(new_letter)
